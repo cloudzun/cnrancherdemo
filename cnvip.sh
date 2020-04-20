f9fa04b4c4922764b11b508379a19afeed657f3d
CloudZun <info@cloudzun.com>
2020-04-20 18:57:13 +0800

    Create cnvip.sh

---
+++ cnvip.sh
@@ -0,0 +1,15 @@
+az group create --name cnrancherdemo --location chinaeast2
+
+az vm create --resource-group cnrancherdemo --name rancherdemo --public-ip-address-dns-name rancherdemo --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data host.txt  
+az vm open-port --resource-group cnrancherdemo --name rancherdemo --port 443 --priority 1001
+az vm open-port --resource-group cnrancherdemo --name rancherdemo --port 80 --priority 1002
+az vm open-port --resource-group cnrancherdemo --name rancherdemo --port 5601 --priority 1003
+
+az vm create --resource-group cnrancherdemo --name ranchersrv1 --public-ip-address-dns-name ranchersrv1 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_D4s_v3 --custom-data node.txt
+az vm open-port --resource-group cnrancherdemo --name ranchersrv1 --port 0-65535 --priority 1001
+
+az vm create --resource-group cnrancherdemo --name ranchersrv2 --public-ip-address-dns-name ranchersrv2 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_D4s_v3 --custom-data node.txt
+az vm open-port --resource-group cnrancherdemo --name ranchersrv2 --port 0-65535 --priority 1001
+
+az vm create --resource-group cnrancherdemo --name ranchersrv3 --public-ip-address-dns-name ranchersrv3 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_D4s_v3 --custom-data node.txt
+az vm open-port --resource-group cnrancherdemo --name ranchersrv3 --port 0-65535 --priority 1001
