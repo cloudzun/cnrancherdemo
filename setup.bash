88255a66caed92d9d6f8aaa00ed2576822e96a76
CloudZun <info@cloudzun.com>
2020-04-20 12:35:05 +0800

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,14 +1,20 @@
 az group create --name cnrancherdemo --location chinaeast2
-az vm create --resource-group cnrancherdemo --name rancherdemo --public-ip-address-dns-name rancherdemo --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_B2ms --custom-data host.txt  
+
+az vm create --resource-group cnrancherdemo --name rancherdemo --public-ip-address-dns-name rancherdemo --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data host.txt  
 az vm open-port --resource-group cnrancherdemo --name rancherdemo --port 443 --priority 1001
 az vm open-port --resource-group cnrancherdemo --name rancherdemo --port 80 --priority 1002
 
+az vm create --resource-group cnrancherdemo --name ranchersrv1 --public-ip-address-dns-name ranchersrv1 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data node.txt
+az vm open-port --resource-group cnrancherdemo --name ranchersrv1 --port 0-65535 --priority 1001
 
-az vm create --resource-group cnrancherdemo --name ranchernode1 --public-ip-address-dns-name ranchernode1 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_B2ms --custom-data node.txt
-az vm open-port --resource-group cnrancherdemo --name ranchernode1 --port 0-65535 --priority 1001
+az vm create --resource-group cnrancherdemo --name ranchersrv2 --public-ip-address-dns-name ranchersrv2 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data node.txt
+az vm open-port --resource-group cnrancherdemo --name ranchersrv2 --port 0-65535 --priority 1001
 
-az vm create --resource-group cnrancherdemo --name ranchernode2 --public-ip-address-dns-name ranchernode2 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_B2ms --custom-data node.txt
-az vm open-port --resource-group cnrancherdemo --name ranchernode2 --port 0-65535 --priority 1001
+az vm create --resource-group cnrancherdemo --name ranchersrv3 --public-ip-address-dns-name ranchersrv3 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B2ms --custom-data node.txt
+az vm open-port --resource-group cnrancherdemo --name ranchersrv3 --port 0-65535 --priority 1001
 
-az vm create --resource-group cnrancherdemo --name ranchernode3 --public-ip-address-dns-name ranchernode3 --image UbuntuLTS --admin-username chengzh --authentication-type password --admin-password Azure1234567890+ --size  Standard_B2ms --custom-data node.txt
-az vm open-port --resource-group cnrancherdemo --name ranchernode3 --port 0-65535 --priority 1001
+
+
+
+
+
