01fdc9b11a046ac22f1cb135597845cf81a7fff8
CloudZun <info@cloudzun.com>
2020-04-20 17:40:57 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 cd
 rm -rf cnrancherdemo
 az group delete -n  cnrancherdemo --yes
+ssh-keygen -f "/home/chengzh/.ssh/known_hosts" -R "rancherdemo.chinaeast2.cloudapp.chinacloudapi.cn"
+ssh-keygen -f "/home/chengzh/.ssh/known_hosts" -R "ranchersrv1.chinaeast2.cloudapp.chinacloudapi.cn"
+ssh-keygen -f "/home/chengzh/.ssh/known_hosts" -R "ranchersrv2.chinaeast2.cloudapp.chinacloudapi.cn"
+ssh-keygen -f "/home/chengzh/.ssh/known_hosts" -R "ranchersrv3.chinaeast2.cloudapp.chinacloudapi.cn"
